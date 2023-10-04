7603ad745b77b11853b6bcc2084a24a478f81536
Hannah Kim <devhkim13@gmail.com>
2023-10-04 15:54:32 -0700

    Add and style logo icon next to header title

--- src/dom/header.js
+++ src/dom/header.js
@@ -1,10 +1,14 @@
+import logoImg from '../img/logo.svg';
 import appendChildren from '../logic/helper-functions';
 
 const createHeader = () => {
+  const logo = document.createElement('img');
+  logo.src = logoImg;
+  logo.alt = 'Partly cloudy and sunny icon';
   const heading = document.createElement('h1');
   heading.textContent = 'Weather Forecast';
   const header = document.createElement('header');
-  appendChildren(header, heading);
+  appendChildren(header, logo, heading);
 
   return header;
 };
